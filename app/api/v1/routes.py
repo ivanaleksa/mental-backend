5ba45efe96052c27f7bad567fa60b882cf544f0c
Ivan <ivan.aleksandrov03@mail.ru>
2025-04-30 20:17:45 +0400

    chore: add user registration method

--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -1,8 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.schemas.user import UserCreate, UserResponse
+from app.db.session import get_db
+from app.services.user_service import register_user_service
+
 
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/healthcheck")
 async def hello_world():
     return {"message": "Hello, world!"}
+
+
+@router.post("/register", response_model=UserResponse)
+async def register_user(user_data: UserCreate, db: AsyncSession = Depends(get_db)):
+    try:
+        return await register_user_service(user_data, db)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
---
+++ app/db/models/__init__.py
@@ -0,0 +1,7 @@
+from .client import Client
+from .psychologist import Psychologist
+from .admin import Admin
+from .note import Note
+from .confirmation_request import ConfirmationRequest
+from .psychologist_request import PsychologistRequest
+from .client_request import ClientRequest
