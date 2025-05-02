60b3cfe72faa49171346839c04b3646bb392faec
Ivan <ivan.aleksandrov03@mail.ru>
2025-05-02 14:07:50 +0400

    chore: change api routes and add dummy endpoint for new confrimation sending

--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -17,10 +17,10 @@ async def hello_world():
     return {"message": "Hello, world!"}
 
 
-@router.post("/register", response_model=UserResponse)
+@router.post("/user/register", response_model=UserResponse)
 async def register_user(user_data: UserCreate, db: AsyncSession = Depends(get_db)):
     """
-    Refister a new user and return their info with a JWT token.
+    Register a new user and return their info with a JWT token.
     """
     try:
         return await register_user_service(user_data, db)
@@ -28,7 +28,7 @@ async def register_user(user_data: UserCreate, db: AsyncSession = Depends(get_db
         raise HTTPException(status_code=400, detail=str(e))
 
 
-@router.post("/login", response_model=UserResponse)
+@router.post("/user/login", response_model=UserResponse)
 async def login_user(login_data: UserLogin, db: AsyncSession = Depends(get_db)):
     """
     Login a user and return their info with a JWT token.
@@ -39,7 +39,7 @@ async def login_user(login_data: UserLogin, db: AsyncSession = Depends(get_db)):
         raise HTTPException(status_code=400, detail=str(e))
 
 
-@router.post("/change-password", response_model=UserResponse)
+@router.post("/user/change-password", response_model=UserResponse)
 async def change_password(update_info: UserUpdatePassword, db: AsyncSession = Depends(get_db)):
     """
     Change the password of a user.
@@ -50,7 +50,7 @@ async def change_password(update_info: UserUpdatePassword, db: AsyncSession = De
         raise HTTPException(status_code=400, detail=str(e))
 
 
-@router.post("/confirm-email/{code}")
+@router.post("/email/confirm/{code}")
 async def confirm_email(code: str, current_user: Client = Depends(get_current_user),
                         db: AsyncSession = Depends(get_db)):
     """
@@ -60,3 +60,12 @@ async def confirm_email(code: str, current_user: Client = Depends(get_current_us
         return await confirm_email_service(db, code, current_user.client_id)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.post("/email/send-new")
+async def send_new_email_confirmation(current_user: Client = Depends(get_current_user),
+                                      db: AsyncSession = Depends(get_db)):
+    """
+    Send a new email confirmation email in the case of needing one more (for example, the previous is expired)
+    """
+    pass
