3eef5c8b84dc8ced7785462b13baeb4d19769546
Ivan <ivan.aleksandrov03@mail.ru>
2025-05-05 12:28:57 +0400

    db: add new migration for ClientRequest

--- app/db/models/client_request.py
+++ app/db/models/client_request.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from datetime import datetime, timezone
+
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.dialects.postgresql import ENUM as PgEnum
 from sqlalchemy.orm import relationship
 
@@ -14,5 +16,8 @@ class ClientRequest(Base):
     client_id = Column(Integer, ForeignKey("clients.client_id"), nullable=False)
     document = Column(String, nullable=False)
     status = Column(PgEnum(RequestStatusEnum, name="request_status", create_type=False), nullable=False)
+    rejection_reason = Column(String, nullable=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.now(timezone.utc))
+    updated_at = Column(DateTime, nullable=False, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
 
     client = relationship("Client", back_populates="client_requests")
