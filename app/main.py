e2bba113eba35d3f8f4ccc335821077935ff1b1c
Ivan <ivan.aleksandrov03@mail.ru>
2025-05-06 16:50:12 +0400

    chore: add cors policy

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 import os
 
 from fastapi import FastAPI
-from app.core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
+from app.core.config import settings
 from app.api.v1.auth_routes import router as api_router
 from app.api.v1.user_routes import router as user_router
 from app.api.v1.admin_routes import router as admin_router
@@ -20,6 +21,13 @@ def ensure_directories():
 ensure_directories()
 
 app = FastAPI(title="Mental Platform")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_router, prefix="/app/v1")
 app.include_router(user_router, prefix="/app/v1")
 app.include_router(admin_router, prefix="/app/v1")
